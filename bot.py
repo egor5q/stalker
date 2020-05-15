f5ecb69c7d1ca5eaf380667a741984af378566a0
egor5q <31898567+egor5q@users.noreply.github.com>
2020-05-15 19:51:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -128,9 +128,9 @@ streets = {
 
 # locs.remove({'code':'shop_street'})
 
-@bot.message_handler(func = lambda m: m.json['dice'] != None)
+@bot.message_handler(func = lambda m: m.dice != None)
 def dicess(m):
-    bot.send_message(441399484, m.json['dice'])
+    bot.send_message(441399484, m.dice)
 
 for ids in streets:
     street = streets[ids]
