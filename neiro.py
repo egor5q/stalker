393840c45012a98baa876990da31f97febf7c57f
egor5q <31898567+egor5q@users.noreply.github.com>
2020-05-18 23:20:43 +0300

    Update neiro.py

--- neiro.py
+++ neiro.py
@@ -15,30 +15,33 @@ import numpy as np
 #print(syn0)
 #print(syn1)
 
-def nonlin(x, deriv = False):
-    if(deriv == True):
-        return f(x)*(1-f(x))
-    return 1/(1+np.exp(-x))
-X = np.array([
-    [0, 0, 1],
-    [0, 1, 1],
-    [1, 0, 1],
-    [1, 1, 1]
-])
 
-y = np.array([[0, 0, 1, 1]]).T
 
-np.random.seed(1)
-syn0 = 2*np.random.random((3, 1)) - 1
-
-for item in range(10000):
-    l0 = X
-    l1 = nonlin(np.dot(l0, syn0))
-    l1_error = y - l1
-    
-    l1_delta = l1_error * nonlin(l1, True)
-    
-    syn0 += np.dot(l0.T, l1_delta)
-    
-print('Выходные данные после тренировки: ')
-print(l1)
+#def nonlin(x, deriv = False):
+#    if(deriv == True):
+#        return f(x)*(1-f(x))
+#    return 1/(1+np.exp(-x))
+#
+#X = np.array([
+#    [0, 0, 1],
+#    [0, 1, 1],
+#    [1, 0, 1],
+#    [1, 1, 1]
+#])
+#
+#y = np.array([[0, 0, 1, 1]]).T
+#
+#np.random.seed(1)
+#syn0 = 2*np.random.random((3, 1)) - 1
+#
+#for item in range(10000):
+#    l0 = X
+#    l1 = nonlin(np.dot(l0, syn0))
+#    l1_error = y - l1
+#    
+#    l1_delta = l1_error * nonlin(l1, True)
+#    
+#    syn0 += np.dot(l0.T, l1_delta)
+#    
+#print('Выходные данные после тренировки: ')
+#print(l1)
