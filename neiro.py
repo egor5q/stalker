08bd3d2af74c0b39ccb83a63b3bb1c8b8cd7c9b7
egor5q <31898567+egor5q@users.noreply.github.com>
2020-05-18 23:05:28 +0300

    Update neiro.py

--- neiro.py
+++ neiro.py
@@ -4,7 +4,7 @@ X = np.array([ [0,0,1],[0,1,1],[1,0,1],[1,1,1] ])
 y = np.array([[0,1,1,0]]).T
 syn0 = 2*np.random.random((3,4)) - 1
 syn1 = 2*np.random.random((4,1)) - 1
-for j in xrange(60000):
+for j in range(60000):
     l1 = 1/(1+np.exp(-(np.dot(X,syn0))))
     l2 = 1/(1+np.exp(-(np.dot(l1,syn1))))
     l2_delta = (y - l2)*(l2*(1-l2))
