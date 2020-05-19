40d2729c64d3726c68662a25244e3f391ffd277b
egor5q <31898567+egor5q@users.noreply.github.com>
2020-05-20 01:49:16 +0300

    Update neiro2.py

--- neiro2.py
+++ neiro2.py
@@ -17,5 +17,5 @@ db=client.neirotalk2
 
 import nltk
 nltk.download('averaged_perceptron_tagger')
-tokens = nltk.word_tokenize('Лошадь жуёт траву.')
-print(nltk.pos_tag(tokens))
+tokens = 'Лошадь жуёт траву.'
+print(nltk.pos_tag(tokens.split()))
