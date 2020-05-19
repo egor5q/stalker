c4ec9650b41e04642fdcd7cbaf3acc881cfe4f6b
egor5q <31898567+egor5q@users.noreply.github.com>
2020-05-20 02:56:31 +0300

    Update neiro2.py

--- neiro2.py
+++ neiro2.py
@@ -113,6 +113,9 @@ def check_predlozh(text, newword = False):
     return True
         
     
+def medit(message_text, chat_id, message_id, reply_markup=None, parse_mode=None):
+    return bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=message_text, reply_markup=reply_markup,
+                                 parse_mode=parse_mode)
     
 def check():
     threading.Timer(15, check).start()
