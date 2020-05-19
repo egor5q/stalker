acccb23d9362c01d91854be87546d62b6a96ef6b
egor5q <31898567+egor5q@users.noreply.github.com>
2020-05-20 01:39:58 +0300

    Update neiro2.py

--- neiro2.py
+++ neiro2.py
@@ -16,4 +16,5 @@ client=MongoClient(os.environ['database'])
 db=client.neirotalk2
 
 import nltk
+nltk.download('averaged_perceptron_tagger')
 print(nltk.pos_tag("Machine learning is great".split()))
