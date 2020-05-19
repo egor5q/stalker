cc176a090d410c22a139696de48950191dd2cac2
egor5q <31898567+egor5q@users.noreply.github.com>
2020-05-20 00:51:59 +0300

    Update neiro2.py

--- neiro2.py
+++ neiro2.py
@@ -1,1 +1,16 @@
 
+import os
+import telebot
+import time
+import random
+import threading
+from emoji import emojize
+from telebot import types
+from pymongo import MongoClient
+import traceback
+    
+token = os.environ['neiro']
+bot = telebot.TeleBot(token)
+
+client=MongoClient(os.environ['database'])
+db=client.neirotalk2
