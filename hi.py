583ef8d4c7edcccfd5b0cddbb96fe76ee5fd90f5
egor5q <31898567+egor5q@users.noreply.github.com>
2020-05-30 14:38:31 +0300

    Update hi.py

--- hi.py
+++ hi.py
@@ -13,7 +13,7 @@ bot = TeleBot(os.environ['hi'])
 def stikk(m):
     print(m.sticker.file_id)
 
-@bot.message_handler(content_types = ['new_chat_member'])
+@bot.message_handler(content_types = ['new_chat_members'])
 def newmwm(m):
     stik = 'CAACAgIAAxkBAAMCXtJFY5hU1RjGiF-2TfdqH_qB1QcAAh8AA51v8gKBOa_rqLp0lRkE'
     bot.send_sticker(m.chat.id, stik, reply_to_message_id = m.message_id) 
