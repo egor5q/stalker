5de17772bd425af25481a2867f5c089f26a11baa
egor5q <31898567+egor5q@users.noreply.github.com>
2020-05-30 14:37:18 +0300

    Update hi.py

--- hi.py
+++ hi.py
@@ -15,6 +15,6 @@ def stikk(m):
 
 @bot.message_handler(content_types = ['new_chat_member'])
 def newmwm(m):
-    stik = ''
+    stik = 'CAACAgIAAxkBAAMCXtJFY5hU1RjGiF-2TfdqH_qB1QcAAh8AA51v8gKBOa_rqLp0lRkE'
     bot.send_sticker(m.chat.id, stik, reply_to_message_id = m.message_id) 
 
