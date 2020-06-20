74f25270fe6a2f7b3918ac22211ec1231ea462d9
egor5q <31898567+egor5q@users.noreply.github.com>
2020-06-20 20:55:48 +0300

    Update steal_zhabka.py

--- steal_zhabka.py
+++ steal_zhabka.py
@@ -118,6 +118,8 @@ def see_pos(player, loc, code):
             print('here')
             if 'wall' not in loc[str(x+i-1)+'_'+str(y+i)]['objects'] and 'wall' not in loc[dot]['objects']:
                 print('here2')
+                print(code)
+                print(str(x+i)+'_'+str(y+i+1))
                 if code == str(x+i)+'_'+str(y+i+1):
                     print('here3')
                     see = True
