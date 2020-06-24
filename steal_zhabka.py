6455a3659fec4b2c4defb7038eddd5c8a5f0e600
egor5q <31898567+egor5q@users.noreply.github.com>
2020-06-24 20:53:56 +0300

    Update steal_zhabka.py

--- steal_zhabka.py
+++ steal_zhabka.py
@@ -425,14 +425,13 @@ def calls(call):
         else:
             bot.answer_callback_query(call.id, 'Вы ещё не можете ходить!')
             if player['callback'] != '':
-            try:
-                bot.answer_callback_query(call.id, 'Новости:\n\n'+game['players'][ids]['callback'], show_alert = True)
+                try:
+                    bot.answer_callback_query(call.id, 'Новости:\n\n'+game['players'][ids]['callback'], show_alert = True)
                 
-            except:
-                bot.answer_callback_query(call.id, 'Новости:\n\n'+'Слишком много текста!', show_alert = True)
-            player['callback'] = ''
+                except:
+                    bot.answer_callback_query(call.id, 'Новости:\n\n'+'Слишком много текста!', show_alert = True)
+                player['callback'] = ''
                 
-            game['players'][ids]['callback'] = ''
             return
         
     except:
