f25ab7ec2e072a70c17e5b78dabbfcdde5c2dca4
egor5q <31898567+egor5q@users.noreply.github.com>
2020-06-30 17:53:32 +0300

    Delete hi.py

--- hi.py
+++
@@ -1,8 +0,0 @@
-from telebot import TeleBot
-import config
-bot = TeleBot(config.environ['hi'])
-
-
-@bot.message_handler(content_types=['new_chat_members'])
-def newmwm(m):
-    bot.send_sticker(m.chat.id, config.hello_sticker, reply_to_message_id=m.message_id)
